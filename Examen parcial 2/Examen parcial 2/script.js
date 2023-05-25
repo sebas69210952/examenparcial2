4c0eb3657e2413f32f67fd054bd4b3bd7eec5ef9
sebas69210952 <132626189+sebas69210952@users.noreply.github.com>
2023-05-24 22:02:50 -0400

    Add files via upload

---
+++ Examen parcial 2/Examen parcial 2/script.js
@@ -0,0 +1,51 @@
+
+/*Primero resuelve el problema. Luego, escribe el código.
+*/
+
+/*Cualquier código tuyo que no hayas mirado en los últimos seis meses o más,
+es como si lo hubiese escrito otra persona
+
+-- Eagleson's Law*/
+
+let uno = true;
+let dos = "2";
+let tres = 3;
+let cuatro = null;
+let cinco;
+let seis = {numero: 6};
+let siete = [7];
+
+console.log(typeof uno); 
+console.log(typeof "dos"); 
+console.log(typeof tres ); 
+console.log(typeof cuatro); 
+console.log(typeof cinco); 
+console.log(typeof seis);
+console.log(typeof siete);
+
+let nombreproducto = "celular";
+let precio = 1500;
+let direccionEnvio = "cañoto";
+let pais = "españa";
+let costoEnvio = 500;
+
+let liberia = "React es una biblioteca Javascript de código abierto creada por Facebook";
+
+let string = "liberia";
+let firstLetter = string[0,6 ];
+
+let lastIndex = string.length - 1;
+
+console.log(lastIndex); 
+console.log(string[lastIndex]);
+
+string.replace("React es una biblioteca");
+
+let string1 = "React es una biblioteca";
+console.log(string.replace("React es una biblioteca", "ReactNative es un framewor")); 
+
+let countries = "TOYOTA, SUZUKI, BMW, FORD, KIA, ISUZU, AUDI";
+
+console.log(countries.split(',')); 
+console.log(countries.split(','));
+
